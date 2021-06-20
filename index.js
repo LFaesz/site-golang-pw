548b977437d37f73300d2f8da577d3860aeb816c
Lucas Faes <lucasfaes2010@gmail.com>
2021-06-20 01:19:49 -0300

    Comentando as funcoes do javascript

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import home from "./pages/home.js"
+import home from "./pages/home.js"; //Importando as paginas para fazer a manipulação dos conteudos com o codigo abaixo
 import sumario from "./pages/sumario.js";
 import cont1 from "./pages/cont1.js";
 import cont2 from "./pages/cont2.js";
 import cont3 from "./pages/cont3.js";
 
-const main = document.querySelector("#root");
+const main = document.querySelector("#root"); // Está pegando a div com id="root" do index.html 
 
-const init = () => {
-    window.addEventListener("hashchange", () => { // Essa ação vai verificar o link, exemplo: caso link = #sumario, o main do index.html recebe o template sumario.js
-        main.innerHTML = "";
+const init = () => { // Função executada após o load da pagina home
+    window.addEventListener("hashchange", () => { // Essa ação vai verificar a hashtag da url, exemplo: caso hashtag = #sumario, a div main do index.html recebe o template sumario.js
+        main.innerHTML = ""; // Essa função é para que não fique adicionando template um em baixo do outro, por exemplo, sempre que eu entrar em uma pagina diferente, primeiro vai limpar a tela e depois adicionar esse template na tela
         switch(window.location.hash){ // caso url = #sumario, o main do index.html recebe o template do /pages/sumario.js
             case "":
-                main.appendChild(home());
+                main.appendChild(home()); //appendChild -> Adicionando um "filho" no elemento main, que no caso é a div#root do index.html
                 break;
             case "#sumario":
                 main.appendChild(sumario());
@@ -25,13 +25,13 @@ const init = () => {
             case "#cont3":
                 main.appendChild(cont3());
                 break;
-            default:
+            default: // Essa função é para caso a # não exista, vai diretamente para a pagina home, poderia também ser uma pagina de erro por exemplo
                 main.appendChild(home());
         }
     })
 }
 
-window.addEventListener("load", () => {
-    main.appendChild(home());
-    init();
+window.addEventListener("load", () => { // Esse código é para printar na tela o template home ao carregar a pela 1º vez pagina
+    main.appendChild(home()); // appendChild -> Adicionando um "filho" no elemento main, que no caso é a div#root do index.html
+    init(); // Esse init() é para executar a função la de cima, ou seja, primeiro vai carregar a pagina home e dps executar o codigo de cima
 })
